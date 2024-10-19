8313e7ca30bd38d6c4f781b18b09776645d8bfc8
dhurteau26 <dhurteau26@gmail.com>
2024-10-19 15:59:05 -0400

    added authentication

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,28 +3,45 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    return token ? jwtDecode<JwtPayload>(token) : null;
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return token;
   }
   
   isTokenExpired(token: string) {
-    // TODO: return a value that indicates if the token is expired
+    // Decode the token to get its expiration time
+    const decoded: JwtPayload = jwtDecode(token);
+    // Check if the token is expired
+    if (decoded.exp && decoded.exp < Date.now() / 1000) {
+      return true;
+    }
+    return false;
+    
   }
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
   }
 }
 
